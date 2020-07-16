e516b4945bf28ef335413b0f9cf32c9fb01a6bc3
Carl Björknäs <carl.bjorknas@knowit.se>
2020-07-16 13:26:40 +0200

    Can create new Employee. Employee edits are saved to db.

--- BethanysPieShopHRM.Server/Pages/EmployeeEditBase.cs
+++ BethanysPieShopHRM.Server/Pages/EmployeeEditBase.cs
@@ -27,15 +27,67 @@ namespace BethanysPieShopHRM.Server.Pages
         public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
         protected string JobCategoryId = string.Empty;
 
+        // Used to store state of screen
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
         protected override async Task OnInitializedAsync()
         {
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Countries = (await CountryDataService.GetAllCountries()).ToList();
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            Employee = int.TryParse(EmployeeId, out var parsedEmployeeId)
+                ? await EmployeeDataService.GetEmployeeDetails(parsedEmployeeId)
+                : CreateDefaultEmployee();
+
             CountryId = Employee.CountryId.ToString();
             JobCategoryId = Employee.JobCategoryId.ToString();
+        }
 
-            Countries = (await CountryDataService.GetAllCountries()).ToList();
+        private Employee CreateDefaultEmployee() => 
+            new Employee
+            {
+                CountryId = 1,
+                JobCategoryId = 1,
+                BirthDate = DateTime.Now,
+                JoinedDate = DateTime.Now
+            };
 
-            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+        protected async Task HandleValidSubmit()
+        {
+            Employee.CountryId = int.Parse(CountryId);
+            Employee.JobCategoryId = int.Parse(JobCategoryId);
+
+            if (Employee.EmployeeId == 0)
+            {
+                var addedEmployee = await EmployeeDataService.AddEmployee(Employee);
+                if (addedEmployee != null)
+                {
+                    StatusClass = "alert-success";
+                    Message = "New employee added successfully.";
+                    Saved = true;
+                }
+                else
+                {
+                    StatusClass = "alert-danger";
+                    Message = "Something went wrong when adding the new employee. Please try again.";
+                    Saved = false;
+                }
+            }
+            else
+            {
+                await EmployeeDataService.UpdateEmployee(Employee);
+                StatusClass = "alert-success";
+                Message = "Employee updated successfully.";
+                Saved = true;
+            }
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+            StatusClass = "alert-danger";
+            Message = "There are some validation errors. Please try again.";
         }
     }
 }
